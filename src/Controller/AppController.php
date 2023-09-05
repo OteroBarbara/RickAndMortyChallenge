0d5118472df722d07463dfcb25e8850e7aa12eb6
Barbara Otero <oterobarbara.251@gmail.com>
2023-09-05 18:25:26 -0300

    primer version ejercicio 2

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Api\RickAndMortyApiService;
+use App\Service\EpisodeLocationService;
 use App\Service\CharCounterService;
 use Symfony\Component\Stopwatch\Stopwatch;
 use App\Utils\TimeFormatter;
@@ -20,24 +21,24 @@ class AppController extends AbstractController
     }
 
     #[Route('/', name: 'home')]
-    public function index(CharCounterService $charCounter): JsonResponse
+    public function index(CharCounterService $charCounter, EpisodeLocationService $episodeLocation): JsonResponse
     {
         $stopwatch = new Stopwatch();
         $stopwatch->start('char_count');
 
-        $locationData = $this->rickAndMortyApiService->getLocations();
-        $episodeData = $this->rickAndMortyApiService->getEpisodes();
+        $locationsData = $this->rickAndMortyApiService->getLocations();
+        $episodesData = $this->rickAndMortyApiService->getEpisodes();
         $charactersData = $this->rickAndMortyApiService->getCharacters();
 
         $results = [
             [
                 "char" => "l",
-                "count" => $charCounter->getCount($locationData,"name",'l'),
+                "count" => $charCounter->getCount($locationsData,"name",'l'),
                 "resource" => "location",
             ],
             [
                 "char" => "e",
-                "count" => $charCounter->getCount($episodeData,"name",'e'),
+                "count" => $charCounter->getCount($episodesData,"name",'e'),
                 "resource" => "episode",
             ],
             [
@@ -47,15 +48,23 @@ class AppController extends AbstractController
             ],
         ];        
 
+
+        $exercise2 = $episodeLocation->getLocations($episodesData,$charactersData);
+        
         $event = $stopwatch->stop('char_count');
         $executionTime = $event->getDuration();
         $formattedTime = TimeFormatter::millisecondsToTimeString($executionTime);
 
+        
         return new JsonResponse([
-            "exercise_name" => "Char counter",
+            /* "exercise_name" => "Char counter",
             "time" => $formattedTime,
             "in_time" => $executionTime < 3000,
-            "results" => $results
+            "results" => $results, */
+            "exercise_name" => "Episode locations",
+            "time" => $formattedTime,
+            "in_time" => $executionTime < 3000,
+            "results" => $exercise2,
         ]);
     }
 }
--- src/Service/EpisodeLocationService.php
+++ src/Service/EpisodeLocationService.php
@@ -4,5 +4,35 @@ namespace App\Service;
 
 class EpisodeLocationService
 {
-    
+    public function getLocations(array $jsonEpisodesList, array $jsonCharactersList)
+    {
+        $results = [];
+        // Recorrer la lista    
+        foreach ($jsonEpisodesList as $jsonEpisode) {
+
+            $characterLocations = [];
+
+            // Obtener el valor, es un endpoint donde consultar la información del personaje
+            $charactersUrls = $jsonEpisode["characters"];
+
+            foreach ($charactersUrls as $url) {
+                // Ya disponemos de la lista de personajes, así que nos quedamos con la parte de la url que representa el ID
+                $characterId = basename(strrchr($url, "/"));
+                // Con el ID consultamos la locación de origen del personaje en el arreglo que disponemos
+                array_push($characterLocations,$jsonCharactersList[$characterId-1]["origin"]["name"]);
+            }
+
+            // Eliminamos los duplicados y lo convertimos en un arreglo indexado (sin claves)
+            $characterLocations = array_values(array_unique($characterLocations));
+
+            array_push($results, [
+                "name" => $jsonEpisode["name"],
+                "episode" => $jsonEpisode["episode"],
+                "cantLocations" => sizeof($characterLocations),
+                "locations" => $characterLocations,
+            ]);
+        }
+        
+        return $results;
+    }
 }
